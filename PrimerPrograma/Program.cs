123706b4463c2f94de09e367911f142f2f1905cc
MauriR511 <00095120@uca.edu.sv>
2021-03-11 17:52:37 -0600

    Primer Programa

---
+++ PrimerPrograma/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PrimerPrograma
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //declarando variables
+            int a = 0, b = 0;
+
+            //Pidiendo el valor para "a"
+            Console.WriteLine("Enter a value for a:");
+            a = Int32.Parse(Console.ReadLine());
+
+            //Pidiendo el valor para "b"
+            Console.WriteLine("Enter a value for b:");
+            b = Int32.Parse(Console.ReadLine());
+
+            //Mostrando la suma y la respuesta
+            Console.WriteLine($"Adding {a} + {b} equals {AddNumbers(a, b)} ");
+        }
+
+        //Funcion que realiza la suma
+        public static int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
